1d6a500b8595a79c144cf21a96ed7835b39394b1
Matt <45981739+Thinkr3@users.noreply.github.com>
2020-02-24 16:17:11 -0600

    Edit style

--- Ch03/Syntax/MovingDisk.java
+++ Ch03/Syntax/MovingDisk.java
@@ -17,38 +17,37 @@ public class MovingDisk extends JPanel
   public MovingDisk()
   {
     time = 0;
-    Timer clock = new Timer(30, this);
+    final Timer clock = new Timer(30, this);
     clock.start();
   }
 
-  public void paintComponent(Graphics g)
-  {
-    int x = 150 - (int)(100 * Math.cos(0.005 * Math.PI * time));
-    int y = 130 - (int)(75 * Math.sin(0.005 * Math.PI * time));
-    int r = 20;
+  public void paintComponent(final Graphics g) {
+    final int x = 150 - (int) (100 * Math.cos(0.005 * Math.PI * time));
+    final int y = 130 - (int) (75 * Math.sin(0.005 * Math.PI * time));
+    final int r = 20;
 
     Color sky;
-    if (y > 130) sky = Color.BLACK;
-    else sky = Color.CYAN;
+    if (y > 130)
+      sky = Color.BLACK;
+    else
+      sky = Color.CYAN;
     setBackground(sky);
     super.paintComponent(g);
 
     g.setColor(Color.ORANGE);
-    g.fillOval(x - r, y - r, 2*r, 2*r);
+    g.fillOval(x - r, y - r, 2 * r, 2 * r);
   }
 
-  public void actionPerformed(ActionEvent e)
-  {
+  public void actionPerformed(final ActionEvent e) {
     time++;
     repaint();
   }
 
-  public static void main(String args)
-  {
-    JFrame w = new JFrame("Moving Disk");
+  public static void main(final String args) {
+    final JFrame w = new JFrame("Moving Disk");
     w.setSize(300, 150);
 
-    Container c = w.getContentPane();
+    final Container c = w.getContentPane();
     c.add(new MovingDisk());
 
     w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
