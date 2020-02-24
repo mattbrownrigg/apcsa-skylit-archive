e2693123bd88eaeff7794b00a354d798e7bd1436
Matt <45981739+Thinkr3@users.noreply.github.com>
2020-02-24 15:57:22 -0600

    Finish Ch03 Syntax and Fix package Ch03 Syntax

--- Ch03/Syntax/MovingDisk.java
+++ Ch03/Syntax/MovingDisk.java
@@ -1,3 +1,5 @@
+package Ch03.Syntax;
+
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Graphics;
@@ -7,26 +9,26 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class MovingDisk extands JPanel
+public class MovingDisk extends JPanel
                   implements ActionListener
 {
   private int time;
 
   public MovingDisk()
   {
-    time = 0
+    time = 0;
     Timer clock = new Timer(30, this);
-    clock.start;
+    clock.start();
   }
 
   public void paintComponent(Graphics g)
   {
     int x = 150 - (int)(100 * Math.cos(0.005 * Math.PI * time));
-    int y = 130 - (int)75 * Math.sin(0.005 * Math.PI * time));
+    int y = 130 - (int)(75 * Math.sin(0.005 * Math.PI * time));
     int r = 20;
 
     Color sky;
-    if (y > 130) sky = Color.BLACK
+    if (y > 130) sky = Color.BLACK;
     else sky = Color.CYAN;
     setBackground(sky);
     super.paintComponent(g);
@@ -43,11 +45,11 @@ public class MovingDisk extands JPanel
 
   public static void main(String args)
   {
-    JFrame w = new JFrame("Moving Disk);
+    JFrame w = new JFrame("Moving Disk");
     w.setSize(300, 150);
 
     Container c = w.getContentPane();
-    c.add(new movingDisk());
+    c.add(new MovingDisk());
 
     w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     w.setResizable(false);
